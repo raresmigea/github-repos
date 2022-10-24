34aafb02731c4d38264569196dafac86db3d65f6
raresmigea <raresini@yahoo.com>
2022-10-24 21:13:23 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import data from './data/repos.json'; 
+
+interface Account {
+  name: string;
+  stars: number;
+  forks: number;
+  link: string;
+}
 
 function App() {
+
+  const [list, setList] = useState<Array<Account>|null>(null);
+
+  useEffect(()=> 
+  setList(data), [])
+
+  console.log('LIST: ', list);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
     </div>
   );
 }
