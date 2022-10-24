6b8b9ace29dcc043a1f1420e460154ae284d25f7
raresmigea <raresini@yahoo.com>
2022-10-24 22:05:21 +0300

    typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableContainer, TablePagination } from '@mui/material';
+import { Paper, Table, TableContainer, TablePagination, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import List from './components/list';
@@ -14,7 +14,7 @@ export interface Account {
 function App() {
 
   const [list, setList] = useState<Array<Account>|[]>([]);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   const [page, setPage] = useState(0);
   useEffect(()=> 
   setList(data), [])
@@ -33,13 +33,14 @@ function App() {
   return (
     <div style={{display: 'flex', margin: '20px'}}>
        <Paper sx={{ width: '100%' }}>
+        <Typography variant="h4" style={{margin: '10px'}}>Github repos list</Typography>
          <TableContainer sx={{ maxHeight: 440 }}>
           <Table stickyHeader aria-label="sticky table">
              {list.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(element => <List {...element} />)}
           </Table>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={[5, 10, 20]}
           component="div"
           count={list.length}
           rowsPerPage={rowsPerPage}
